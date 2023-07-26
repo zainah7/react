a217eceb26d01746f26d30db3c9cb23958321215
zainah7 <zainaabd7@gmail.com>
2023-07-26 21:51:11 +0100

    update

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom";
 import { AiFillDelete } from "react-icons/ai";
+import { useHistory } from "react-router-dom";
+
 const BlogList = ({ blogs }) => {
+  const history = useHistory();
+  const deleteBlog = (blogId) => {
+    fetch("http://localhost:8000/blogs/" + blogId, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-list">
       {blogs.map((blog) => (
-        <div>
-          <div className="blog-preview" key={blog.id}>
-            <Link to={`/blogs/${blog.id}`}>
-              <div className="Delete">
-                <h2>{blog.title}</h2>
-                <AiFillDelete className="icon" />
-              </div>
-
-              <p>Written by {blog.author}</p>
-            </Link>
+        <div className="blog-preview" key={blog.id}>
+          <Link to={`/blogs/${blog.id}`}>
+            <div className="Delete">
+              <h2>{blog.title}</h2>{" "}
+            </div>
+            <p>Written by {blog.author}</p>
+          </Link>
+          <div onClick={() => deleteBlog(blog.id)}>
+            <AiFillDelete className="icon" />
           </div>
         </div>
       ))}
